7220410f6228746f513a6812c97aefc7eca41c01
xD'Criz <32378563+criz16@users.noreply.github.com>
2022-11-16 21:11:53 +0800

    Update twscript.sh

--- twscript.sh
+++ twscript.sh
@@ -8,10 +8,10 @@ GREEN='\033[01;32m';
 WHITE='\033[01;37m';
 YELLOW='\033[00;33m';
 clear
-echo -e "$GREEN   CHOOSE YOUR SERVER PLAN:"
+echo -e "$RED   CHOOSE YOUR SERVER PLAN:"
 echo "";
 echo "";
-echo "Type of your Server" 
+echo "Type of your Server"
 PS3='Choose or Type a Plan: '
 echo
 options=("Premium" "VIP" "Private" "Quit")
@@ -23,7 +23,7 @@ echo "";
 wget -O /usr/local/sbin/ssh.php https://raw.githubusercontent.com/criz16/tw/main/premium.sh -q
 #############################
 echo "";
-echo "1) Premium Selected";
+echo -e "$RED	1) Premium Selected";
 break ;;
 VIP,*|*,VIP) 
 echo "";
@@ -31,7 +31,7 @@ echo "";
 wget -O /usr/local/sbin/ssh.php https://raw.githubusercontent.com/criz16/tw/main/vip.sh -q
 #############################
 echo "";
-echo "2) VIP Selected";
+echo -e "$RED	2) VIP Selected";
 break ;;
 Private,*|*,Private) 
 echo "";
@@ -39,16 +39,17 @@ echo "";
 wget -O /usr/local/sbin/ssh.php https://raw.githubusercontent.com/criz16/tw/main/private.sh -q
 #############################
 echo "";
-echo "3) Private Selected";
+echo -e "$RED	3) Private Selected";
 sleep 3s
 break ;;
 Quit,*|*,Quit) echo "Installation Cancelled!!";
-echo "\e[1;31mRebuild your vps and correct the process.\e[0m";
+echo -e "\e[1;31mRebuild your vps and correct the process.\e[0m";
 exit;
 break ;; *)
 echo Invalid: Choose a proper Plan;;
 esac
 done
+
 
 
 
@@ -108,12 +109,15 @@ tput cnorm
 
 show_menu () {
 echo -e "                $GREEN
+
+
 ░█████╗░██████╗░██╗███████╗
 ██╔══██╗██╔══██╗██║╚════██║
 ██║░░╚═╝██████╔╝██║░░███╔═╝
 ██║░░██╗██╔══██╗██║██╔══╝░░
 ╚█████╔╝██║░░██║██║███████╗
 ░╚════╝░╚═╝░░╚═╝╚═╝╚══════╝     
+
 $RESET"
 }
 install(){
@@ -209,9 +213,11 @@ cat << XYZZY > /etc/systemd/system/stunnel.service
 [Unit]
 Description=SSL tunnel for network daemons
 After=syslog.target
+
 [Service]
 ExecStart=/usr/bin/stunnel
 Type=forking
+
 [Install]
 WantedBy=multi-user.target
 XYZZY
