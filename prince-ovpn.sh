a2aa8f21d8f52ee532b799605b5f56a718cd5086
xD'Criz <32378563+criz16@users.noreply.github.com>
2023-01-02 18:45:52 +0800

    Update prince-ovpn.sh

--- prince-ovpn.sh
+++ prince-ovpn.sh
@@ -372,10 +372,10 @@ verb 3' > /etc/openvpn/server2.conf
 
 cat <<\EOM >/etc/openvpn/login/config.sh
 #!/bin/bash
-HOST='131.226.4.9'
-USER='teamworkvpn_relly'
-PASS='EhMx#Hg3Bnvg'
-DB='teamworkvpn_database'
+HOST='131.226.4.13'
+USER='twjhe_admin'
+PASS='jYWJ9l9nb9yp'
+DB='twjhe_database'
 EOM
 
 /bin/cat <<"EOM" >/etc/openvpn/login/auth_vpn
