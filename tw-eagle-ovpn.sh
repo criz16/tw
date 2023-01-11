937922bce6888cdea22d76e7d5a12d7999f7baab
xD'Criz <32378563+criz16@users.noreply.github.com>
2023-01-11 22:11:31 +0800

    Update tw-eagle-ovpn.sh

--- tw-eagle-ovpn.sh
+++ tw-eagle-ovpn.sh
@@ -372,10 +372,10 @@ verb 3' > /etc/openvpn/server2.conf
 
 cat <<\EOM >/etc/openvpn/login/config.sh
 #!/bin/bash
-HOST='198.251.89.144'
-USER='jhejheon_tweagle'
-PASS='p#Z[4pN9fMX='
-DB='jhejheon_tweagle'
+HOST='131.226.4.9'
+USER='romnick_admin'
+PASS='ncyIQ9riacoI'
+DB='romnick_database'
 EOM
 
 /bin/cat <<"EOM" >/etc/openvpn/login/auth_vpn
